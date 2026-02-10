defec1d7ff67a6bba732c4518e6ea16655995fbf
Vyshu <kvsvdevi@gmail.com>
2026-02-10 19:11:46 +0530

    Day 1 of challenge

---
+++ 30_days_of_python/day_01.py
@@ -0,0 +1,23 @@
+#introduction
+# Day 1 - 30 DaysOfPython Challenge 
+
+print("hello world!")
+
+#operation -arthimetic
+
+print(2+3) #addition
+print(3-2) #subtraction
+print(2*3) #multiplication
+print(56/2) #division
+print(2**3) #power
+print(2%3) #gives remainder
+print(1234//10)  #gets last number (floor division)
+
+#datatypes
+
+print(type(10))
+print(type("hello"))
+print(type([1,2,3,4,5]))
+print(type(1+4j))
+print(type(1213.54767))
+print(type(True))
